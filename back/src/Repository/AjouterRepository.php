721d11a4b9226c735c0b4c2bd984cb939b35398b
Pozymandiaz <lucasverstraete01@gmail.com>
2023-07-20 08:45:04 +0200

    Ajout des readme

---
+++ back/src/Repository/AjouterRepository.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Ajouter;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Ajouter>
+ *
+ * @method Ajouter|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Ajouter|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Ajouter[]    findAll()
+ * @method Ajouter[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AjouterRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Ajouter::class);
+    }
+
+    public function save(Ajouter $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Ajouter $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function getSalesFromSevenDays()
+    {
+        $result = array();
+        $date = new \DateTime();
+
+        for ($i = 0; $i < 7; $i++) {
+            $formattedDate = $date->format('Y-m-d');
+            $count = $this->createQueryBuilder('a')
+                ->select('SUM(a.qte)')
+                ->andWhere('a.date = :formattedDate')
+                ->setParameter('formattedDate', $formattedDate)
+                ->getQuery()
+                ->getSingleScalarResult();
+
+            $result[$formattedDate] = $count;
+
+            $date->modify('-1 day');
+        }
+
+        return $result;
+    }
+
+    public function getSalesFromFiveWeeks()
+    {
+        $result = array();
+        $date = new \DateTime();
+
+        for ($i = 0; $i < 5; $i++) {
+            $weekStart = clone $date;
+            $weekStart->modify('Monday this week')->modify('-' . ($i * 7) . ' days');
+            $weekEnd = clone $weekStart;
+            $weekEnd->modify('Sunday this week');
+
+            $formattedWeekStart = $weekStart->format('Y-m-d');
+            $formattedWeekEnd = $weekEnd->format('Y-m-d');
+
+            $count = $this->createQueryBuilder('c')
+                ->select('SUM(c.qte)')
+                ->andWhere('c.date >= :weekStart')
+                ->andWhere('c.date <= :weekEnd')
+                ->setParameter('weekStart', $formattedWeekStart)
+                ->setParameter('weekEnd', $formattedWeekEnd)
+                ->getQuery()
+                ->getSingleScalarResult();
+
+            $result[$formattedWeekStart] = $count;
+        }
+
+        return $result;
+    }
+
+    public function getSalesFromSevenDaysCategory()
+    {
+        $result = array();
+        $date = new \DateTime();
+
+        $qb = $this->createQueryBuilder('a');
+        $qb->select('c.nom')
+            ->leftJoin('a.produit', 'p')
+            ->leftJoin('p.categorie', 'c')
+            ->orderBy('c.nom', 'ASC')
+            ->addGroupBy('c.nom', 'a.date')
+            ->distinct(true);
+
+        $query = $qb->getQuery();
+        $results = $query->getResult();
+
+        for ($i = 0; $i < 7; $i++) {
+            foreach ($results as $row) {
+                $category = $row['nom'];
+                $formattedDate = $date->format('Y-m-d');
+
+                $count = $this->createQueryBuilder('a')
+                    ->select('SUM(a.qte)')
+                    ->leftJoin('a.produit', 'p')
+                    ->leftJoin('p.categorie', 'c')
+                    ->andWhere('a.date = :formattedDate')
+                    ->andWhere('c.nom = :category')
+                    ->setParameter('formattedDate', $formattedDate)
+                    ->setParameter('category', $category)
+                    ->getQuery()
+                    ->getSingleScalarResult();
+
+                $result[$formattedDate][$category] = $count;
+            }
+            $date->modify('-1 day');
+        }
+        return $result;
+    }
+
+    public function getSalesFromFiveWeeksCategory()
+    {
+
+        $result = array();
+        $date = new \DateTime();
+
+        $qb = $this->createQueryBuilder('a');
+        $qb->select('c.nom')
+            ->leftJoin('a.produit', 'p')
+            ->leftJoin('p.categorie', 'c')
+            ->orderBy('c.nom', 'ASC')
+            ->addGroupBy('c.nom', 'a.date')
+            ->distinct(true);
+
+        $query = $qb->getQuery();
+        $results = $query->getResult();
+
+        for ($i = 0; $i < 7; $i++) {
+            $weekStart = clone $date;
+            $weekStart->modify('Monday this week')->modify('-' . ($i * 7) . ' days');
+            $weekEnd = clone $weekStart;
+            $weekEnd->modify('Sunday this week');
+
+            $formattedWeekStart = $weekStart->format('Y-m-d');
+            $formattedWeekEnd = $weekEnd->format('Y-m-d');
+            foreach ($results as $row) {
+                $category = $row['nom'];
+
+                $count = $this->createQueryBuilder('a')
+                    ->select('SUM(a.qte)')
+                    ->leftJoin('a.produit', 'p')
+                    ->leftJoin('p.categorie', 'c')
+                    ->andWhere('a.date >= :weekStart')
+                    ->andWhere('a.date <= :weekEnd')
+                    ->setParameter('weekStart', $formattedWeekStart)
+                    ->setParameter('weekEnd', $formattedWeekEnd)
+                    ->andWhere('c.nom = :category')
+                    ->setParameter('category', $category)
+                    ->getQuery()
+                    ->getSingleScalarResult();
+
+                $result[$formattedWeekStart][$category] = $count;
+            }
+        }
+        return $result;
+    }
+
+}
---
+++ bdd/e-commerce (1).sql
@@ -0,0 +1,342 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 12 juil. 2023 à 16:03
+-- Version du serveur : 10.4.27-MariaDB
+-- Version de PHP : 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `e-commerce`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `adresse`
+--
+
+CREATE TABLE `adresse` (
+  `id` int(11) NOT NULL,
+  `intitule` varchar(250) NOT NULL,
+  `ville` varchar(200) NOT NULL,
+  `region` varchar(200) NOT NULL,
+  `code_postal` varchar(5) NOT NULL,
+  `pays` varchar(200) NOT NULL,
+  `utilisateur_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ajouter`
+--
+
+CREATE TABLE `ajouter` (
+  `id` int(11) NOT NULL,
+  `produit_id` int(11) NOT NULL,
+  `commande_id` int(11) NOT NULL,
+  `qte` int(11) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+CREATE TABLE `categorie` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `categorie`
+--
+
+INSERT INTO `categorie` (`id`, `nom`) VALUES
+(6, 'test'),
+(7, 'lit'),
+(8, 'canape'),
+(9, 'bureau'),
+(10, 'armoire');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande`
+--
+
+CREATE TABLE `commande` (
+  `id` int(11) NOT NULL,
+  `statut` varchar(100) NOT NULL,
+  `utilisateur_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `compose`
+--
+
+CREATE TABLE `compose` (
+  `id` int(11) NOT NULL,
+  `materiaux_id` int(11) NOT NULL,
+  `produit_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `image`
+--
+
+CREATE TABLE `image` (
+  `id` int(11) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `produit_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `materiaux`
+--
+
+CREATE TABLE `materiaux` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `materiaux`
+--
+
+INSERT INTO `materiaux` (`id`, `nom`) VALUES
+(4, 'future');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit`
+--
+
+CREATE TABLE `produit` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(150) NOT NULL,
+  `prix` float NOT NULL,
+  `stock` int(10) NOT NULL,
+  `description` text NOT NULL,
+  `carousel` tinyint(1) NOT NULL DEFAULT 0,
+  `highlander` tinyint(1) NOT NULL DEFAULT 0,
+  `categorie_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `produit`
+--
+
+INSERT INTO `produit` (`id`, `nom`, `prix`, `stock`, `description`, `carousel`, `highlander`, `categorie_id`) VALUES
+(6, 'test', 10, 10, 'test', 1, 1, 6),
+(7, 'a', 11, 33, 'test2', 1, 0, 8),
+(8, 'b', 20, 66, 'test3', 1, 0, 9),
+(9, 'c', 5, 100, 'test4', 1, 1, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(150) NOT NULL,
+  `prenom` varchar(150) NOT NULL,
+  `tel` varchar(10) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `mdp` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `tel`, `email`, `mdp`) VALUES
+(3, 'Bertreux', 'Stanislas', '0000000000', 'bertreux.stanislas@gmail.com', 'i');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `adresse`
+--
+ALTER TABLE `adresse`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQUE` (`utilisateur_id`),
+  ADD KEY `utilisateur_id` (`utilisateur_id`) USING BTREE;
+
+--
+-- Index pour la table `ajouter`
+--
+ALTER TABLE `ajouter`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_produit` (`produit_id`) USING BTREE,
+  ADD KEY `id_commande` (`commande_id`) USING BTREE;
+
+--
+-- Index pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_utilisateur` (`utilisateur_id`) USING BTREE;
+
+--
+-- Index pour la table `compose`
+--
+ALTER TABLE `compose`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_materiaux` (`materiaux_id`) USING BTREE,
+  ADD KEY `id_produit` (`produit_id`) USING BTREE;
+
+--
+-- Index pour la table `image`
+--
+ALTER TABLE `image`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `materiaux`
+--
+ALTER TABLE `materiaux`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_categorie` (`categorie_id`) USING BTREE;
+
+--
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `adresse`
+--
+ALTER TABLE `adresse`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `ajouter`
+--
+ALTER TABLE `ajouter`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT pour la table `commande`
+--
+ALTER TABLE `commande`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `compose`
+--
+ALTER TABLE `compose`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `image`
+--
+ALTER TABLE `image`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `materiaux`
+--
+ALTER TABLE `materiaux`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `produit`
+--
+ALTER TABLE `produit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `adresse`
+--
+ALTER TABLE `adresse`
+  ADD CONSTRAINT `cle_utili` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `ajouter`
+--
+ALTER TABLE `ajouter`
+  ADD CONSTRAINT `cle_commande` FOREIGN KEY (`commande_id`) REFERENCES `commande` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `cle_produit` FOREIGN KEY (`produit_id`) REFERENCES `produit` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD CONSTRAINT `cle_utilisateur` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `compose`
+--
+ALTER TABLE `compose`
+  ADD CONSTRAINT `cle_materiaux` FOREIGN KEY (`materiaux_id`) REFERENCES `materiaux` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `cle_produi` FOREIGN KEY (`produit_id`) REFERENCES `produit` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD CONSTRAINT `cle_categorie` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
